564cb6bf9a80b2af8802d9dfa3c9dc411f6859a2
Deri Kurniawan <deri.netuchi@gmail.com>
2022-07-12 00:12:49 +0700

    add sundanese lang

--- src/locales/languages.js
+++ src/locales/languages.js
@@ -7,6 +7,7 @@ const cl = (code, flag, dir, name) => {
 const languages = [
   cl('en', 'us', 'ltr', 'English',),
   cl('id', 'id', 'ltr', 'Bahasa Indonesia'),
+  cl('sunda', 'id', 'ltr', 'Basa Sunda'),
   cl('fr', 'fr', 'ltr', 'Français'),
   cl('ae', 'ae', 'rtl', 'العربية'),
 ];
