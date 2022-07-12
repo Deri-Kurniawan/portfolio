79fe69003c003e5ccc9afedbcbd15369dfd4619f
Deri Kurniawan <deri.netuchi@gmail.com>
2022-07-13 01:15:12 +0700

    update

--- src/locales/languages.js
+++ src/locales/languages.js
@@ -2,10 +2,10 @@ import cl from './lib/createLanguage'
 
 const languages = [
   cl('en', 'us', 'ltr', 'English'),
-  cl('id', 'id', 'ltr', 'Bahasa Indonesia'),
+  cl('id', 'id', 'ltr', 'Indonesia'),
   cl('fr', 'fr', 'ltr', 'Français'),
   cl('ae', 'ae', 'rtl', 'العربية'),
-  cl('ids', 'id', 'ltr', 'Basa Sunda'),
+  cl('ids', 'id', 'ltr', 'Sunda'),
 ];
 
 export const supportedLanguages = languages.map((lang) => lang.code); 
